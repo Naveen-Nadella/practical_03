97706a7f03f71a6fef5541b2342dbed3b597d8c2
Naveen-Nadella <naveennadella02@gmail.com>
2025-08-08 14:02:51 +0530

    Update App.jsx

--- crud_frontend/crud_frontend-main/src/App.jsx
+++ crud_frontend/crud_frontend-main/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   const handleChange = (e) => {
     setProduct({ ...product, [e.target.name]: e.target.value });
   };
-const BASE_URL = 'http://184.72.122.226:9090/springapp1';
+const BASE_URL = 'http://18.234.246.81:9090/springapp1';
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
